c5dcaf71f5ff21782ddc5d0730c160281d0bff38
Matt Copperwaite <618054+yamatt@users.noreply.github.com>
2025-12-01 21:52:50 +0000

    lint fix on nosec

--- ddor/rss.py
+++ ddor/rss.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from email.utils import format_datetime
 from functools import cached_property
 
-import xml.etree.ElementTree as ET # nosec
+import xml.etree.ElementTree as ET  # nosec
 
 
 class RSSBuilder:

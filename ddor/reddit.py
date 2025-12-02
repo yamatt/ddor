bcb6b6c1628eac4b439f83dd622bc247ad965659
Matt Copperwaite <618054+yamatt@users.noreply.github.com>
2025-12-02 12:44:59 +0000

    better logging

--- ddor/reddit.py
+++ ddor/reddit.py
@@ -1,8 +1,10 @@
 from functools import cached_property
 
 import requests
+from requests.exceptions import HTTPError
 from ratelimit import limits, sleep_and_retry
 
+from .logger import log
 
 class Reddit:
     URL_TEMPLATE = "http://www.reddit.com/r/{subreddit}/top/.json?t=day"
@@ -28,7 +30,11 @@ class Reddit:
     def get_json(self, url):
         response = self.session.get(url)
 
-        response.raise_for_status()
+        try:
+            response.raise_for_status()
+        except HTTPError as e:
+            log.error("JSON REQUEST", success=False, status=response.status_code, message=response.text, request=response.request.headers)
+            raise e
 
         return response.json()
 
